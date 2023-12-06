35e80c57542681f8add16e9c3e3f4e34045432cb
mafizul247 <mafizul247@gmail.com>
2023-12-06 13:19:05 +0600

    Update Portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,9 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <div className='max-w-7xl mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
 )
